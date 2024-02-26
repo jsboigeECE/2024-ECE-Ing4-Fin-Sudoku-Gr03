5df254aeb1f4f35d81f7d784d885cf65e743cdab
Jean-Sylvain Boige <jsboige@gmail.com>
2024-02-26 23:21:58 +0100

    Réincorporation des différents opérateurs génétiques

--- Sudoku.GeneticAlgorithm/GeneticSolver.cs
+++ Sudoku.GeneticAlgorithm/GeneticSolver.cs
@@ -21,7 +21,7 @@ namespace Sudoku.GeneticAlgorithm
             return sdkBoard;
         }
     }
-    /*
+
     public class GeneticPermutedCellsCycleTworsSolver : ISudokuSolver
     {
         public SudokuGrid Solve(SudokuGrid s)
@@ -31,9 +31,9 @@ namespace Sudoku.GeneticAlgorithm
             var popSize = 400;
 
             var crossover = new CycleCrossover();
-            
+
             var mutation = new TworsMutation();
-           
+
             var sdkBoard = SudokuTestHelper.Eval(permutatedCellsChromosome, crossover, mutation, s, popSize);
 
             return sdkBoard;
@@ -49,7 +49,7 @@ namespace Sudoku.GeneticAlgorithm
             var popSize = 400;
 
             var crossover = new PartiallyMappedCrossover();
-            
+
             var mutation = new ReverseSequenceMutation();
 
             var sdkBoard = SudokuTestHelper.Eval(permutatedCellsChromosome, crossover, mutation, s, popSize);
@@ -67,9 +67,9 @@ namespace Sudoku.GeneticAlgorithm
             var popSize = 400;
 
             var crossover = new OrderedCrossover();
-            
+
             var mutation = new InsertionMutation();
-            
+
             var sdkBoard = SudokuTestHelper.Eval(permutatedCellsChromosome, crossover, mutation, s, popSize);
 
             return sdkBoard;
@@ -108,15 +108,15 @@ namespace Sudoku.GeneticAlgorithm
 
             var popSize = 400;
             var crossover = new UniformCrossover();
-            
+
             var mutation = new UniformMutation();
-            
+
             var sdkBoard = SudokuTestHelper.Eval(permutatedCellsChromosome, crossover, mutation, s, popSize);
 
             return sdkBoard;
 
         }
     }
-    */
+
 
 }
