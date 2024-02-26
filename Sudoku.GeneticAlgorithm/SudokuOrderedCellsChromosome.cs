9b5eadcdf0fbdf057ade79ba29a980633226e21b
CamilleSennegon <118520815+CamilleSennegon@users.noreply.github.com>
2024-02-26 19:29:22 +0100

    Solveur final

---
+++ Sudoku.GeneticAlgorithm/SudokuOrderedCellsChromosome.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using GeneticSharp;
+using Sudoku.GeneticAlgorithm;
+using Sudoku.Shared;
+
+namespace Sudoku.GeneticAlgorithm
+{
+    public class SudokuOrderedCellsChromosome : SudokuChromosomeBase, ISudokuChromosome
+    {
+        public IList<(int row, int col)> GeneToCellLookup
+        {
+            get
+            {
+                if (_geneToCellLookup == null)
+                {
+                    if (TargetSudokuGrid == null)
+                    {
+                        _geneToCellLookup = Enumerable.Range(0, 81).Select(x => (x / 9, x % 9)).ToList();
+                    }
+                    else
+                    {
+                        _geneToCellLookup = new List<(int row, int col)>(Length);
+                        for (int i = 0; i < 9; i++)
+                        {
+                            for (int j = 0; j < 9; j++)
+                            {
+                                if (TargetSudokuGrid.Cells[i][j] == 0)
+                                {
+                                    GeneToCellLookup.Add((i, j));
+                                }
+                                else
+                                {
+                                    // Add 1 to the lookup table for each number that is already in the board
+                                    this.baseLookupTable[TargetSudokuGrid.Cells[i][j] - 1]++;
+                                }
+                            }
+                        }
+                    }
+                    this.cloneLookupTable = new List<int>(baseLookupTable);
+                }
+                return _geneToCellLookup;
+            }
+        }
+
+        private List<int> baseLookupTable = new List<int>(9) { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private List<int> cloneLookupTable;
+        private IList<(int row, int col)>? _geneToCellLookup;
+
+        public SudokuOrderedCellsChromosome()
+            : this(null)
+        {
+        }
+
+        public SudokuOrderedCellsChromosome(SudokuGrid? targetSudokuBoard)
+            : this(targetSudokuBoard, null)
+        {
+        }
+
+        public SudokuOrderedCellsChromosome(
+            SudokuGrid? targetSudokuBoard,
+            Dictionary<(int x, int y), List<int>> extendedMask)
+            : base(targetSudokuBoard, extendedMask, targetSudokuBoard?.NbEmptyCells() ?? 81)
+        {
+        }
+
+        public SudokuOrderedCellsChromosome(
+            SudokuGrid targetSudokuBoard,
+            Dictionary<(int x, int y), List<int>> extendedMask,
+            IList<(int row, int col)> objGeneToCellLookup,
+            List<int> objBaseLookupTable)
+            : base(targetSudokuBoard, extendedMask, targetSudokuBoard?.NbEmptyCells() ?? 81)
+        {
+            this._geneToCellLookup = objGeneToCellLookup;
+            this.baseLookupTable = objBaseLookupTable;
+            this.cloneLookupTable = new List<int>(objBaseLookupTable);
+        }
+
+        public static Random Random = new Random();
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            var targetCell = GeneToCellLookup[geneIndex];
+
+            var availableFromLookup = this.cloneLookupTable.Select((value, ind) => (value, ind))
+                .Where((tuple => tuple.value < 9)).Select(tuple => tuple.ind + 1).ToArray();
+            var availableFromCoherence = this.ExtendedMask[(targetCell.row, targetCell.col)];
+            var crossedAvail = availableFromCoherence.Where(i => availableFromLookup.Contains(i)).ToArray();
+            int figureValue;
+            if (crossedAvail.Length == 0)
+            {
+                // If no value is available from the mask and the lookuptable, we take the minimum value (index + 1) from the lookup table
+                figureValue = this.cloneLookupTable.Select((value, ind) => (value, ind)).MinBy(tuple => tuple.value).ind + 1;
+            }
+            else
+            {
+                // If there are values available from the mask and the lookuptable, we take a random value from the crossed list
+                var figureIndex = Random.Next(crossedAvail.Count());
+                figureValue = crossedAvail[figureIndex];
+            }
+
+            var geneValue = cloneLookupTable[figureValue - 1] * 9 + figureValue - 1;
+            cloneLookupTable[figureValue - 1] += 1;
+
+            Gene gene = new Gene(geneValue);
+            return gene;
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return (IChromosome)new SudokuOrderedCellsChromosome(this.TargetSudokuGrid, this.ExtendedMask,
+                GeneToCellLookup, baseLookupTable);
+        }
+
+        public override bool UsesPermutations()
+        {
+            return false;
+        }
+
+        public override IList<SudokuGrid> GetSudokus()
+        {
+            var toReturn = new List<SudokuGrid>();
+            var computedSudoku = TargetSudokuGrid.CloneSudoku();
+            toReturn.Add(computedSudoku);
+            var genes = GetGenes();
+            for (int geneIndex = 0; geneIndex < this.Length; geneIndex++)
+            {
+                var cellIndex = GeneToCellLookup[geneIndex];
+                computedSudoku.Cells[cellIndex.row][cellIndex.col] = (int)genes[geneIndex].Value % 9 + 1;
+            }
+            return toReturn;
+        }
+    }
+}
---
+++ Sudoku.GeneticAlgorithm/SudokuPermutatedCellsChromosome.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using GeneticSharp;
+using Sudoku.GeneticAlgorithm;
+using Sudoku.Shared;
+
+namespace Sudoku.GeneticAlgorithm
+{
+    public class SudokuPermutatedCellsChromosome : SudokuChromosomeBase, ISudokuChromosome
+    {
+        private List<int> baseLookupTable = new List<int>(9) { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private List<int> cloneLookupTable = new List<int>(9) { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        private IList<(int row, int col)>? _geneToCellLookup;
+
+        public SudokuPermutatedCellsChromosome()
+            : this(null)
+        {
+        }
+
+        public SudokuPermutatedCellsChromosome(SudokuGrid? targetSudokuBoard)
+            : this(targetSudokuBoard, null)
+        {
+        }
+
+        public SudokuPermutatedCellsChromosome(
+            SudokuGrid? targetSudokuBoard,
+            Dictionary<(int x, int y), List<int>> extendedMask)
+            : base(targetSudokuBoard, extendedMask, 81)
+        {
+        }
+
+        public SudokuPermutatedCellsChromosome(
+            SudokuGrid targetSudokuBoard,
+            Dictionary<(int x, int y), List<int>> extendedMask,
+            List<int> objBaseLookupTable)
+            : base(targetSudokuBoard, extendedMask, 81)
+        {
+            this.baseLookupTable = objBaseLookupTable;
+            this.cloneLookupTable = new List<int>(objBaseLookupTable);
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            if (this.cloneLookupTable.Count == 0)
+            {
+                this.cloneLookupTable = new List<int>(baseLookupTable);
+            }
+
+            var row = geneIndex / 9;
+            var col = geneIndex % 9;
+
+            Gene gene = new Gene(cloneLookupTable[Permutations[row][col] - 1] * 9 + Permutations[row][col] - 1);
+            cloneLookupTable[Permutations[row][col] - 1] += 1;
+
+            return gene;
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return (IChromosome)new SudokuPermutatedCellsChromosome(this.TargetSudokuGrid, this.ExtendedMask, baseLookupTable);
+        }
+
+        public override bool UsesPermutations()
+        {
+            return true;
+        }
+
+        public override IList<SudokuGrid> GetSudokus()
+        {
+            var genes = GetGenes();
+            var indices = Enumerable.Range(0, 9).ToList();
+            var cellGrid = indices.Select(row => indices.Select(col => (int)genes[row * 9 + col].Value % 9 + 1).ToArray()).ToArray();
+            var sudoku = new SudokuGrid() { Cells = cellGrid };
+            return new List<SudokuGrid>(new[] { sudoku });
+        }
+    }
+}
